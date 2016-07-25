69df282f492afcedd36d2af5250951b7c320eb6a
adarapata <efb.youdie@gmail.com>
2016-07-26 00:15:04 +0900

    first commit

---
+++ Popup.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+
+public class Popup : MonoBehaviour {
+	public enum State { Open, Close, UnUsed }
+
+	public State state { get; private set; }
+	public TweenScale open, close;
+	void Start()
+	{
+		open.Setup(gameObject);
+		open.scaleEndAsObservable.Subscribe(_ => state = State.Open);
+		close.Setup(gameObject);
+		close.scaleEndAsObservable.Subscribe(_ => state = State.Close);
+	}
+
+	public void Open()
+	{
+		open.Play();
+	}
+	public void Close()
+	{
+		close.Play();
+	}
+
+	public void Toggle() {
+		switch(state)
+		{
+		case State.UnUsed:
+		case State.Close:
+			open.Play();
+			break;
+		case State.Open:
+			close.Play();
+			break;
+		}
+	}
+}
